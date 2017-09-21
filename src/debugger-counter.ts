c35a37a8ab74f1dba68e83145ea8b424709f5907
Unknown <samuel.souza.koch@gmail.com>
2017-09-20 22:46:57 -0300

    Showing debuggers for all open documents

--- src/debugger-counter.ts
+++ src/debugger-counter.ts
@@ -1,32 +1,37 @@
-import { StatusBarItem, window, StatusBarAlignment, TextDocument } from "vscode";
+import { StatusBarItem, window, StatusBarAlignment, TextDocument, workspace } from "vscode";
 
 export class DebuggerCounter {
     private _statusBarItem : StatusBarItem;
 
     public updateDebuggerCounter(){
         if(!this._statusBarItem){
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right);
         }
 
+        this._statusBarItem.text =  "Debuggers: " ;
+
         let editor = window.activeTextEditor;
+
         if(!editor){
             this._statusBarItem.hide();
             return;
-        }
-
-        let doc = editor.document;
-
-        if(doc.languageId === 'typescript'){
-            let debuggerCount = this.getDebuggerCount(doc);
-            
-            this._statusBarItem.text = debuggerCount !== 1 ? `${debuggerCount} Debuggers` : '1 Debugger';
-            this._statusBarItem.show();
         } else {
-            this._statusBarItem.hide();
+            let currentDocumentDebuggers = this.getDebuggersCount(editor.document);
+
+            this._statusBarItem.text +=  `This: ${currentDocumentDebuggers} ` ;
+            this._statusBarItem.show();
         }
+
+        if(workspace.textDocuments.length > 1){
+            let allDocumentsDebuggers : number = 0;
+            workspace.textDocuments.forEach(doc => {allDocumentsDebuggers += this.getDebuggersCount(doc)})
+            this._statusBarItem.text +=  `Total: ${allDocumentsDebuggers} ` ;
+        } 
+
+        this._statusBarItem.show();
     }
 
-    public getDebuggerCount(doc: TextDocument) : number {
+    public getDebuggersCount(doc: TextDocument) : number {
         let docContent = doc.getText();
         return docContent != "" ? docContent.split(/\bdebugger\b/g).length > 0 ? docContent.split(/\bdebugger\b/g).length - 1 : docContent.split(/\bdebugger\b/g).length : 0;
     }
